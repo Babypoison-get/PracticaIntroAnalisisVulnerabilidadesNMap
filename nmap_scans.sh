cdb7463aee76e1c125860fdbfd9c6a3d4c1be703
Babypoison-get <70213667+Babypoison-get@users.noreply.github.com>
2020-09-04 12:33:46 -0500

    Add files via upload

---
+++ nmap_scans.sh
@@ -0,0 +1,12 @@
+echo -e "\e[5mScan start!!!\e[0m\n"
+echo -e "\e[91mFind all ports!!!\e[0m" 
+xterm -T "Find all ports!!!" -hold -e nmap scanme.nmap.org &
+echo -e "\e[93mFind the OS!!!\e[0m" 
+xterm -T "Find the OS!!!" -hold -e nmap -O scanme.nmap.org &
+#echo -e "\e[92mFind the DNS information!!!\e[0m"
+#xterm -T "Find the DNS information!!!" -hold -e  &
+echo -e "\e[94mFind possible firewalls!!!\e[0m"
+xterm -T "Find possible firewalls!!!" -hold -e nmap scanme.nmap.org --script firewalk -d &
+echo -e "\e[95mFind if broadcast ping is enabled!!!\e[0m"
+xterm -T "Find if broadcast ping is enabled" -hold -e nmap scanme.nmap.org --script broadcast-ping &
+echo -e "\e[5mDone!!!\e[0m"
